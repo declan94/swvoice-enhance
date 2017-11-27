c2352f412614c6000cc5d9cda089477c7050edd4
陈晔 <chenye94@qq.com>
2017-11-27 22:19:35 +0800

    add some utils

---
+++ utils/plotutil.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plotTimeFreq(f, t, z):
+	plt.pcolormesh(t, f, np.log10(np.abs(z)))
+	plt.title('STFT Magnitude')
+	plt.ylabel('Frequency [Hz]')
+	plt.xlabel('Time [sec]')
+	plt.show()
--- utils/waveutil.py
+++ utils/waveutil.py
@@ -2,4 +2,4 @@ from scipy import signal
 
 def calcTimeFreq(x, windowLen, frameLen):
 	overlap = windowLen - frameLen
-	return signal.stft(x, 'hamming', windowLen, overlap)
+	return signal.stft(x, fs=8000, window="hamming", nperseg=windowLen, noverlap=overlap)
