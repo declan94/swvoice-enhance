c427007a61defbf9d6ef2391a1539dc9b288da66
陈晔 <chenye94@qq.com>
2017-11-28 11:23:58 +0800

    mel

--- utils/ioutil.py
+++ utils/ioutil.py
@@ -1,7 +1,9 @@
 import scipy.io.wavfile as wavfile
 
 def loadWaveFile(filepath):
-	data = wavfile.read(filepath)
-	if len(data) != 2:
-		return None
-	return data[1][:, 0]
+    sr, wav = wavfile.read(filepath)
+    assert(sr == 8000)
+    if wav.ndim > 1:
+        return wav[:, 0]
+    else:
+        return wav
--- utils/plotutil.py
+++ utils/plotutil.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plotTimeFreq(f, t, z):
-	plt.pcolormesh(t, f, np.log10(np.abs(z)))
-	plt.title('STFT Magnitude')
-	plt.ylabel('Frequency [Hz]')
-	plt.xlabel('Time [sec]')
-	plt.show()
+    plt.pcolormesh(t, f, np.log10(np.abs(z)))
+    plt.title('STFT Magnitude')
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.show()
